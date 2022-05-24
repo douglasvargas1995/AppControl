831fafe806ca5517899064e4e366655e4bb8a3b6
DOUGLAS <douglas.vargas@universo.univates.br>
2022-05-24 20:03:11 -0300

    versão 01 - 24052022 2001

---
+++ app/src/main/java/com/example/appcontrolautentication/OperadorActivity.java
@@ -0,0 +1,14 @@
+package com.example.appcontrolautentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OperadorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_operador);
+    }
+}
